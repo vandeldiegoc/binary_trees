55d16b0d4ae4b477c9681404a00bed58cc08b695
Diego <vandeldiegoc@gmail.com>
2020-07-23 02:04:11 -0500

    1[add] condition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,11 +8,11 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-
+binary_tree_t *node_left = NULL;
 if (parent == NULL)
 	return (NULL);
 
-binary_tree_t *node_left = malloc(sizeof(binary_tree_t));
+node_left = malloc(sizeof(binary_tree_t));
 if (!node_left)
 	return (NULL);
 
