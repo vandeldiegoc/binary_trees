3e1aaed8f3176dc979ae62b00827118bbe6474a8
Diego <vandeldiegoc@gmail.com>
2020-07-23 02:10:25 -0500

    2[fix] betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,10 +11,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 binary_tree_t *node_right = NULL;
 if (parent == NULL)
 	return (NULL);
-
 node_right = malloc(sizeof(binary_tree_t));
 if (!node_right)
-	return(NULL);
+	return (NULL);
 node_right->parent = parent;
 node_right->left = NULL;
 node_right->right = NULL;
